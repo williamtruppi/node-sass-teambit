4744d8507888c534f841f128485416e946ad41ad
William Truppi <williamt@hotmail.it>
2021-01-27 17:02:14 +0100

    inserita navbar fixed allo scroll

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,6 +11,21 @@ var app = new Vue({
   el: ".wrapper",
   components: {}
 });
+$(function () {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= 650) {
+      $("nav").css('position', 'fixed');
+      $("nav").css('min-width', '100%');
+      $("nav").css('padding-left', '20px');
+      $("nav").css('padding-right', '20px');
+    } else {
+      $("nav").css('position', '');
+      $("nav").css('min-width', '');
+      $("nav").css('padding-left', '');
+      $("nav").css('padding-right', '');
+    }
+  });
+});
 
 /***/ }),
 
--- src/main.js
+++ src/main.js
@@ -6,3 +6,21 @@ let app = new Vue ({
     
   }
 })
+
+$(function () {
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= 650) {
+      $("nav").css('position','fixed');
+      $("nav").css('min-width','100%');
+      $("nav").css('padding-left','20px');
+      $("nav").css('padding-right','20px');
+    } else {
+      $("nav").css('position','');
+      $("nav").css('min-width','');
+      $("nav").css('padding-left','');
+      $("nav").css('padding-right','');
+    }
+  })
+
+});
